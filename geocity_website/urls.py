b930a62a8dd3b7149fef85647e4813cfa062d11c
nicolas-sitylb <nbl@ylb.ch>
2021-01-04 14:21:13 +0000

    Refactor and add some urls

--- geocity_website/urls.py
+++ geocity_website/urls.py
@@ -1,27 +1,31 @@
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, url, path
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
+from wagtail.core import urls as wagtail_urls
 
 from search import views as search_views
+#from .api import api_router
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
+    url(r'django-admin/', admin.site.urls),
 
-    path('admin/', include(wagtailadmin_urls)),
-    path('documents/', include(wagtaildocs_urls)),
+    url(r'admin/', include(wagtailadmin_urls)),
+    url(r'documents/', include(wagtaildocs_urls)),
 
-    path('search/', search_views.search, name='search'),
-
+    url(r'search/', search_views.search, name='search'),
+    url(r'^api/v2/', api_router.urls),
 ]
 
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic import TemplateView
+    from django.views.generic.base import RedirectView
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
@@ -31,7 +35,7 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("", include(wagtail_urls)),
+    url(r'', include(wagtail_urls)),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
